9ff37eb96ff525167227b394027a7a957569b4db
armelant <56300747+armelant@users.noreply.github.com>
2024-04-16 02:38:49 +0300

    recreate the main page and add some styles

--- universe-in-your-pocket/src/App.js
+++ universe-in-your-pocket/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import main from './pages/mainPage.js';
+import "./styles/main.css"
+import MainPage from './pages/mainPage.js';
 
 const App = () => {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<main />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
     </BrowserRouter>
   );
--- universe-in-your-pocket/src/pages/mainPage.js
+++ universe-in-your-pocket/src/pages/mainPage.js
@@ -1,10 +1,31 @@
+import "../styles/mainPage.css"
 
 
 const MainPage = () => {
   return (
-    <div>
-      <h1>Main Page</h1>
-    </div>
+    <> 
+      <div className="contentBox-1">
+          <div className="contentBoxTitle">
+            <h1>Exploring The Vast Universe</h1>
+          </div>
+
+            <div className="contentBoxContent">
+              <p>Dive into the infinite wonders of space through our portal. Discover the latest space missions, news, maps, calendar events</p>
+            
+            <button>Get Started</button>
+            </div>
+
+      </div>
+      {/* <div className="contentBox-2">
+        <div className="contentBoxContent">
+          <p>Unleash the mysteries of the cosmos from the comfort of your home</p>
+        </div>
+
+        <div className="contentBoxButton">
+          <button>Get Started</button>
+        </div>
+      </div> */}
+    </>
   );
 };
 
