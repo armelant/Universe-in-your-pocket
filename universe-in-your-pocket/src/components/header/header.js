aba1e5c35e2a8514fc8217616a3219d3a70662f3
AndreiSusha <Andrey.Susha@gmail.com>
2024-04-18 20:39:44 +0300

    feat: Added Blog link

--- universe-in-your-pocket/src/components/header/header.js
+++ universe-in-your-pocket/src/components/header/header.js
@@ -1,27 +1,38 @@
-import "./header.css";
+import './header.css';
 
 const Header = () => {
-    return (
+  return (
     <>
       <div className="navbar">
-      <div className="navBrand">
-        <a href="/">
-        <img src={require("../../img/logo.png")} alt="logo" className="logo" />
-        <span className="companyName">COSMOPOCKET</span>
-        </a>
-      </div>
+        <div className="navBrand">
+          <a href="/">
+            <img
+              src={require('../../img/logo.png')}
+              alt="logo"
+              className="logo"
+            />
+            <span className="companyName">COSMOPOCKET</span>
+          </a>
+        </div>
       </div>
       <div className="navMenu">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/News">News</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/News">News</a>
+          </li>
+          <li>
+            <a href="/News">Blog</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
         </ul>
       </div>
-
-
     </>
-    );
+  );
 };
 
 export default Header;
