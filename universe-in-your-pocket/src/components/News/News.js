0f636bfd5cb89535879c0d688cf543b95b24698f
armelant <56300747+armelant@users.noreply.github.com>
2024-04-25 22:15:52 +0300

    some updates

--- universe-in-your-pocket/src/components/News/News.js
+++ universe-in-your-pocket/src/components/News/News.js
@@ -1,19 +1,14 @@
-
-
 const News = () => {
+  const date = [
+    { name: "jhjhhhh", date: "2024" },
+    { name: "jhjhhhh", date: "2024" },
+    { name: "jhjhhhh", date: "2024" },
+    { name: "jhjhhhh", date: "2024" },
+  ];
 
   return (
     <main className="main">
-
-
-
-
-
-
-
-
-
-      {/* <div className="news">
+      <div className="news">
         <div className="newsBox">
           <div className="newsContent">
             <p>
@@ -39,7 +34,7 @@ const News = () => {
           alt="newsImg"
           className="newsImg"
         />
-      </div> */}
+      </div>
     </main>
   );
 };
