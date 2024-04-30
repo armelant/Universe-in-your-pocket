84e39492882eeb724fce8e119ee31b97f5b3b30b
AndreiSusha <Andrey.Susha@gmail.com>
2024-04-30 22:58:28 +0300

    refactor: beckend side code refactoring

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
 const mongoose = require('mongoose');
@@ -12,30 +13,27 @@ const handleValidationErrors = require('./utils/handleValidationErrors.js');
 const userController = require('./controllers/userController.js');
 const postController = require('./controllers/postController.js');
 
-//Constants
-// const PORT = process.env.PORT;
-// const DB_USER = process.env.DB_USER;
-// const DB_DB_PASSWORD = process.env.DB_DB_PASSWORD;
-// const DB_NAME = process.env.DB_NAME;
-// const CLUSTER = process.env.CLUSTER;
+// Constants;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const CLUSTER = process.env.CLUSTER;
 
-// const dbURI =
-//   'mongodb+srv://' +
-//   process.env.DB_USER +
-//   ':' +
-//   process.env.DB_DB_PASSWORD +
-//   '@' +
-//   process.env.CLUSTER +
-//   '.mongodb.net/' +
-//   process.env.DB_NAME +
-//   '?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI =
+  'mongodb+srv://' +
+  DB_USER +
+  ':' +
+  DB_PASSWORD +
+  '@' +
+  CLUSTER +
+  '.mongodb.net/' +
+  DB_NAME +
+  '?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose
-  .connect(
-    'mongodb+srv://Andrei:Andrei12345@cluster0.ykxhvdv.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0'
-  )
+  .connect(dbURI)
   .then(() => console.log('DB ok'))
-  .catch(() => console.log('DB error', err));
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -98,7 +96,6 @@ app.get('/posts', postController.getAll);
 app.get('/posts/:id', postController.getOne);
 app.post(
   '/posts',
-  checkAuth,
   postCreateValidation,
   handleValidationErrors,
   postController.create
@@ -112,7 +109,7 @@ app.patch(
   postController.update
 );
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     return console.log(err);
   }
